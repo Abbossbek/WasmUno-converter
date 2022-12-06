dc18986dbd050b5316850a136dc0971eb886556c
Abbosbek Niyozqulov <abbos6882@gmail.com>
2022-12-06 15:50:42 +0500

    fix

--- WasmUno.Wasm/Program.cs
+++ WasmUno.Wasm/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+
 using Aspose.Words;
 
 namespace WasmUno.Wasm
@@ -12,7 +13,8 @@ namespace WasmUno.Wasm
     {
         static int Main(string[] args)
         {
-           Console.WriteLine("Hello from C#!");
+            Console.WriteLine("Hello from C#!");
+            Console.WriteLine(typeof(TestClass).Assembly.FullName);
             return 0;
         }
     }
@@ -25,17 +27,29 @@ namespace WasmUno.Wasm
         }
         public static async Task<string> DocToPdf(string id)
         {
-            using (HttpClient client = new())
+            try
             {
-                using (Stream docStream = await client.GetStreamAsync($"https://file.us.uz/download/{id}/content"))
-                using (MemoryStream pdfStream = new())
+                using (HttpClient client = new())
                 {
-                    Document document = new Document(docStream);
-                    //document.FontSettings=new Aspose.Words.Fonts.FontSettings();
-                    //document.FontSettings.SubstitutionSettings.FontConfigSubstitution.Enabled = true;
-                    document.Save(pdfStream, SaveFormat.Pdf);
-                    return Convert.ToBase64String(pdfStream.ToArray());
+                    //client.DefaultRequestHeaders.Add("origin", "*");
+                    //client.DefaultRequestHeaders.Add("access-control-allow-origin", "*");
+                    //client.DefaultRequestHeaders.Add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                    //client.DefaultRequestHeaders.Add("access-control-request-method", "GET, POST, PUT, PATCH, OPTIONS, DELETE");
+                    using (Stream docStream = await client.GetStreamAsync($"https://file.ijro.uz/download/{id}/content"))
+                    using (MemoryStream pdfStream = new())
+                    {
+                        Document document = new Document(docStream);
+                        //document.FontSettings=new Aspose.Words.Fonts.FontSettings();
+                        //document.FontSettings.SubstitutionSettings.FontConfigSubstitution.Enabled = true;
+                        document.Save(pdfStream, SaveFormat.Pdf);
+                        return Convert.ToBase64String(pdfStream.ToArray());
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
             }
         }
     }
